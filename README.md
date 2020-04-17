8f766c71461b6ba91f31ed73acf78749bf80288a
Nicolas R <cpan@atoomic.org>
2020-04-17 09:40:38 -0600

    merge cplay-install

--- README.md
+++ README.md
@@ -2,13 +2,30 @@
 
 # install-cplay
 
-GitHub action to install App::cplay
-
-This action installs 'cplay' as root so you can then use it in your workflow.
+GitHub action to install a perl distribution using App::cplay
 
 ## Inputs
 
-none
+### `install`
+
+List of one or more modules, separated by a newline `\n` character.
+
+### `cpanfile`
+
+Install modules from a cpanfile.
+
+### `tarball`
+
+Install a distribution from a tarball URL or Path.
+
+### `tests`
+
+Boolean variable used to disable unit tests during installation
+Possible values: true | false [default: true]
+
+### `args`
+
+Extra arguments to pass to the cplay command line.
 
 ## Outputs
 
@@ -16,8 +33,77 @@ none
 
 ## Example usage
 
+### Install one module or distribution
+
+```yaml
+- name: Install multiple modules
+  uses: perl-actions/install-cplay@v1.0
+  with:
+    install: Module::One
 ```
-uses: perl-actions/install-cplay@v1.0
-run: |
-   sudo cplay Module::To::Install
+
+You can use either a module name or a distribution name
+
+```yaml
+- name: Install multiple modules
+  uses: perl-actions/install-cplay@v1.0
+  with:
+    install: Distribution-Name
+```
+
+### Install multiple modules
+
+To install more than a single module, use a string seperated by `\n` character.
+
+```yaml
+- name: Install multiple modules
+  uses: perl-actions/install-cplay@v1.0
+  with:
+    install: |
+    	Module::One
+        Another::Module
+```
+
+### Install using a cpanfile
+
+You can use a `cpanfile` to install multiple modules.
+
+```yaml
+- name: Install using a cpanfile
+  uses: perl-actions/install-cplay@v1.0
+  with:
+    cpanfile: "your-cpanfile"
+```
+
+you can also requests some specific rules using the extra `args` argument.
+
+```yaml
+- name: Install using a cpanfile
+  uses: perl-actions/install-cplay@v1.0
+  with:
+    cpanfile: "your-cpanfile"
+    args: "--with-recommends --with-requires"
+```
+
+### Install from a tarball
+
+```yaml
+install_from_tarball:
+  runs-on: ubuntu-latest
+  name: "Install from tarball"
+  steps:
+    - name: install-cplay --from-tarball
+      uses: perl-actions/install-cplay@v1.0
+      with:
+        tarball: "https://github.com/${{ github.repository }}/archive/p5.tar.gz"
+```
+
+### Disabling Unit tests during installation
+
+```yaml
+- name: Install multiple modules
+  uses: perl-actions/install-cplay@v1.0
+  with:
+    install: My::Module
+    tests: false
 ```
--- index.js
+++ index.js
@@ -1,20 +1,76 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const tc = require('@actions/tool-cache');
-const exec = require('@actions/exec');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const tc = require("@actions/tool-cache");
+const exec = require("@actions/exec");
 
-async function action() {
-  const cplay = await tc.downloadTool('https://git.io/cplay');
+async function install_cplay() {
+  const cplay = await tc.downloadTool("https://git.io/cplay");
   core.setOutput("cplay", cplay);
-  await exec.exec( 'sudo', [ 'perl', cplay, 'self-install' ] );
+  await exec.exec("sudo", ["perl", cplay, "self-install"]);
   return;
 }
 
-// Call action
-(async() => {
+async function run() {
+  await install_cplay();
+
+  // input arguments
+  const install = core.getInput("install");
+  const cpanfile = core.getInput("cpanfile");
+  const tests = core.getInput("tests");
+  const args = core.getInput("args");
+  const tarball = core.getInput("tarball");
+
+  const w_test =
+    tests == "true" || tests == "1" || tests == "ok" ? "--test" : "--no-test";
+  var w_args = [];
+
+  if (args !== null && args.length) {
+    w_args = args.split(/\s+/);
+  }
+
+  if (install !== null && install.length) {
+    console.log(`install: ${install}!`);
+    const list = install.split("\n");
+    var cmd = ["cplay", "install", "-d", w_test];
+    if (w_args.length) {
+      cmd = cmd.concat(w_args);
+    }
+    cmd = cmd.concat(list);
+    await exec.exec("sudo", cmd);
+  }
+
+  if (cpanfile !== null && cpanfile.length) {
+    console.log(`cpanfile: ${cpanfile}!`);
+    var cmd = ["cplay", "cpanfile", "-d", w_test];
+    if (w_args.length) {
+      cmd = cmd.concat(w_args);
+    }
+    cmd.push(cpanfile);
+    await exec.exec("sudo", cmd);
+  }
+
+  if (tarball !== null && tarball.length) {
+    console.log(`tarball: ${tarball}!`);
+    var cmd = ["cplay", "from-tarball", "-d", w_test];
+    if (w_args.length) {
+      cmd = cmd.concat(w_args);
+    }
+    cmd.push(tarball);
+    await exec.exec("sudo", cmd);
+  }
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
+
+  return;
+}
+
+// Call run
+(async () => {
   try {
-  	await action();	
+    await run();
   } catch (error) {
-  	core.setFailed(error.message);	
+    core.setFailed(error.message);
   }
 })();
